daabe162a01a960f5740520518dbba680ce6b5e7
zimbatm <zimbatm@zimbatm.com>
2025-09-24 17:02:20 +0200

    WIP: add sops-nix to nuc-002

---
+++ modules/sops.nix
@@ -0,0 +1,11 @@
+{ flakeInputs, config, ... }:
+{
+  imports = [
+    flakeInputs.sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    age.keyFile = "/var/lib/host-identity.key";
+    defaultSopsFile = ../org-config/secrets/hosts/${config.networking.hostName}.json;
+  };
+}
