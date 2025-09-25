100cc57c405c5660f6f19ed997d1c85c412d70af
phaer <phaer@phaer.org>
2025-09-25 14:42:31 +0200

    sops: skip ssh certificates as well
    same as for sk keys, users of those need to add their age identities manually

--- scripts/python_nixostools/nixostools/derive_age_keys.py
+++ scripts/python_nixostools/nixostools/derive_age_keys.py
@@ -21,13 +21,10 @@ def derive_age_keys(
             suffix = "" if index == 0 else f"_{index + 1}"
             age_key_name = f"{username}{suffix}"
             if isinstance(public_key, dict):
-                if "publicKey" in public_key:
-                    public_key = public_key.get("publicKey")
-                else:
-                    logger.warn(
-                        f"WARNING: unsupported key for {age_key_name}: {public_key}, skipping"
-                    )
-                    continue
+                logger.warn(
+                    f"WARNING: unsupported key for {age_key_name}: {public_key}, skipping"
+                )
+                continue
 
             if public_key.startswith("sk-"):
                 logger.warn(
