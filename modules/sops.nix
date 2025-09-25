baa1d5074265220c942db8d07cbfc03fb57b7077
zimbatm <zimbatm@zimbatm.com>
2025-09-25 10:20:19 +0200

    .

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,11 +1,18 @@
-{ flakeInputs, config, ... }:
+{ flakeInputs, config, lib, ... }:
 {
   imports = [
     flakeInputs.sops-nix.nixosModules.sops
   ];
 
   sops = {
+    # TODO: generate and put that into place in install.sh
     age.keyFile = "/var/lib/host-identity.key";
+
+    # Don't use the default SSH host key files for decryption
+    ageSshKeyPaths = lib.mkForce [ ];
+    sshKeyPaths = lib.mkForce [ ];
+
+    # Per host
     defaultSopsFile = ../org-config/secrets/hosts/${config.networking.hostName}.yaml;
   };
 }
