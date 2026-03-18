a0263cae10bd8c50529d43b7ea845149bfffb563
Ian Sollars <69912317+ianso-msf@users.noreply.github.com>
2026-03-18 10:56:35 +0100

    latest from private (#130)

--- modules/system.nix
+++ modules/system.nix
@@ -381,26 +381,10 @@ in
         };
       };
 
-      # For more detail, see:
-      #   https://0pointer.de/blog/projects/watchdog.html
-      watchdog = {
-        # systemd will send a signal to the hardware watchdog at half
-        # the interval defined here, so every 10s.
-        # If the hardware watchdog does not get a signal for 20s,
-        # it will forcefully reboot the system.
-        runtimeTime = 20;
-        # Forcefully reboot if the final stage of the reboot
-        # hangs without progress for more than 30s.
-        # For more info, see:
-        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-        rebootTime = 30;
-      };
-
       sleep.extraConfig = ''
         AllowSuspend=no
         AllowHibernation=no
       '';
-
       services =
         let
           legacy_key_path = "/etc/nixos/local/id_tunnel";
@@ -651,7 +635,7 @@ in
               if [ "$uplink_online" = "1" ]; then
                 echo "Uplink is online, blocking AP from starting"
                 ${pkgs.coreutils}/bin/rm -f "$ALLOW"
-                ${pkgs.systemd}/bin/systemctl stop --no-block hostapd.service dnsmasq.service ap-iface-address.service || true
+                ${pkgs.systemd}/bin/systemctl stop --no-block hostapd.service ap-iface-address.service || true
               else
                 echo "No uplink online, allowing AP"
                 ${pkgs.coreutils}/bin/touch "$ALLOW"
